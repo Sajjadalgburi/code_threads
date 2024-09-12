54ff00a70c074850b183c15c75556ca049f6fcd0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-12 10:43:33 -0400

    Refactor page component to include ModeToggle and update content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { ModeToggle } from "@/components/ThemeSwitch";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <ModeToggle />
+      <h1>awdwd</h1>
+    </div>
+  );
 };
 
 export default page;
