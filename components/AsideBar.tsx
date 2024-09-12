f2a49ecc966bc50c65ea48dafd398610b8082ff5
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-12 11:08:38 -0400

    Add AsideBar component

---
+++ components/AsideBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AsideBar = () => {
+  return (
+    <aside className="aside-bar">
+      <div className="top">
+        <p>logo</p>
+      </div>
+      <div className="mid">
+        <p>item 1</p>
+        <p>item 2</p>
+        <p>item 3</p>
+        <p>item 4</p>
+      </div>
+      <div className="bot">
+        <p>lol</p>
+        <p>lol</p>
+      </div>
+    </aside>
+  );
+};
+
+export default AsideBar;
