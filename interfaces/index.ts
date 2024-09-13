838189396e1cc338f09a71e99bc8af656ac7b1c8
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 09:39:59 -0400

    Add IUser and IThread interfaces for user and thread documents

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,12 @@ export interface IUser extends Document {
   replies?: Schema.Types.ObjectId[];
   threads?: Schema.Types.ObjectId[];
 }
+
+// interface for the thread model/data
+export interface IThread extends Document {
+  text: string;
+  views?: number;
+  likes?: Schema.Types.ObjectId[]; // Array of user IDs
+  replies?: Schema.Types.ObjectId[]; // Array of reply IDs or Thread references
+  user: Schema.Types.ObjectId;
+}
