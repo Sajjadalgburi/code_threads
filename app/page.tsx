6a24bceb1b17f08c3f16156e0072986742a9dc37
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 08:15:25 -0400

    Add Feed and Header components for displaying content

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import AsideBar from "@/components/AsideBar";
+import Feed from "@/components/Feed";
+import Header from "@/components/Header";
 import React from "react";
 
 const page = () => {
@@ -8,9 +10,21 @@ const page = () => {
       <AsideBar />
       <div className="container">
         <div className="wrapper">
-          {" "}
-          <h1>Code Threads</h1>
-          <p>Share cool code snippets and interact with other developers!</p>
+          {/*  1. This will serve the main feed on the left hand side and the 
+          users profile-if they are logged in- on the right hand side 2. The
+          header reusable component will be used in order to display appropriate
+           information on the page */}
+          <div className="bg-blue-500 w-full max-w-3xl rounded-xl">
+            <Header title="for you" />
+            <Feed>awad</Feed>{" "}
+          </div>{" "}
+          {/* This is the users thread/profile which will be dynamically created if the user is currently signed in or not
+
+              EX: {session && <Profile />} */}
+          <div className="bg-blue-500 w-full max-w-3xl rounded-xl">
+            <Header title="Thread" />
+            <Feed>hdadwa</Feed>{" "}
+          </div>
         </div>
       </div>
     </main>
