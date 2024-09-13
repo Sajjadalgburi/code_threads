3e5cd89eceb2be3612a1473e178ea8d19e9d7f20
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 09:22:09 -0400

    Add IUser interface for user documents

---
+++ lib/models/user.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model, models } from "mongoose";
+import { IUser } from "@/interfaces";
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    liked: {
+      type: [Schema.Types.ObjectId],
+      ref: "Thread",
+    },
+    followers: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    replies: {
+      type: [Schema.Types.ObjectId],
+      ref: "Thread",
+    },
+    threads: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thread",
+      },
+    ],
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt fields
+);
+
+// ! thank you gpt-4
+// Explicitly creating indexes
+userSchema.index({ username: 1 }, { unique: true });
+userSchema.index({ email: 1 }, { unique: true });
+
+const User = models.User || model<IUser>("User", userSchema);
+
+export default User;
