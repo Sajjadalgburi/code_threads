f4cb5fcf2e92b175d5842acce3e083e4d61df496
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 11:08:25 -0400

    Add AccessToken field to IUser interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,6 +11,7 @@ export interface IUser extends Document {
   followers?: Schema.Types.ObjectId[];
   replies?: Schema.Types.ObjectId[];
   threads?: Schema.Types.ObjectId[];
+  AccessToken?: any; // ! get back to this once it is figured out
 }
 
 // interface for the thread model/data
