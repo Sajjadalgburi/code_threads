0b7386871f2c5263d202c3e2470c9927bfb59b21
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 08:43:52 -0400

    Refactor page component to separate feed and thread sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,25 @@ import Feed from "@/components/Feed";
 import Header from "@/components/Header";
 import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <main className="flex">
-      {/* Side bar for larger screens */}
+    <main className="flex h-screen">
+      {/* Sidebar for larger screens */}
       <AsideBar />
-      <div className="container">
-        <div className="wrapper">
-          <div className="feed">
-            <Header title="for you" />
-            <Feed>awad</Feed>{" "}
-          </div>
 
-          {/*  */}
-          <div className="feed">
-            <Header title="Thread" />
-            <Feed>hdadwa</Feed>{" "}
-          </div>
+      {/* Main container centered with max-width */}
+      <div className="flex-1 max-w-6xl mx-auto w-full grid grid-cols-2 gap-4 p-4 h-full">
+        <div className="flex flex-col">
+          <Header title="For You" />
+          <Feed>awad</Feed>
+        </div>
+        <div className="flex flex-col">
+          <Header title="Thread" />
+          <Feed>hdadwa</Feed>
         </div>
       </div>
     </main>
   );
 };
 
-export default page;
+export default Page;
