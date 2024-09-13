7191c3c781440497d9538948a4a3f363dc02c0a1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-13 10:52:27 -0400

    Add authentication providers for NextAuthOptions

---
+++ lib/authOptions.ts
@@ -0,0 +1,21 @@
+// these are providers that we will use for authentication
+import GithubProvider from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+// importing the NextAuthOptions type from next-auth
+import { NextAuthOptions } from "next-auth";
+
+// intiliazing the options for the authentication and passing some type references
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
