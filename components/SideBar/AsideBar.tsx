d24584a395060c56eef39ccb13fb7077d1b70d25
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-14 08:37:34 -0400

    Refactor AsideBar component to include logout functionality and conditional rendering based on user session

--- components/SideBar/AsideBar.tsx
+++ components/SideBar/AsideBar.tsx
@@ -5,15 +5,10 @@ import { Menu } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import NavItemButton from "../NavItems";
-import { usePathname } from "next/navigation";
-import { navItems } from "@/constants";
 import { ModeToggle } from "../ThemeSwitch";
-import { signOut, useSession } from "next-auth/react";
+import Nav_Aside from "./nav.aside";
 
 const AsideBar = () => {
-  const pathname = usePathname();
-  const { data: session } = useSession();
-
   return (
     <aside className="aside-bar">
       <div className="top">
@@ -31,40 +26,7 @@ const AsideBar = () => {
         </NavItemButton>{" "}
       </div>
 
-      <nav className="mid">
-        {" "}
-        <ul className="sidebar-nav_elements">
-          {session ? (
-            <>
-              {navItems.map((item) => {
-                const isActive: boolean = item.url === pathname;
-                return (
-                  <li
-                    key={item.id}
-                    className={`${
-                      isActive
-                        ? "bg-purple-gradient text-white bg-slate-300/10 rounded-2xl"
-                        : "text-gray-500"
-                    }`}
-                  >
-                    <NavItemButton>
-                      <Link href={item.url}>
-                        <span
-                          dangerouslySetInnerHTML={{ __html: item.logoSvg }}
-                          className={`${isActive ? "brightness-200" : ""}`}
-                        />{" "}
-                      </Link>
-                    </NavItemButton>
-                  </li>
-                );
-              })}
-              <button onClick={() => signOut()}>Logout</button>
-            </>
-          ) : (
-            <>Please log in</>
-          )}
-        </ul>
-      </nav>
+      <Nav_Aside />
 
       <div className="bot">
         <ModeToggle />
