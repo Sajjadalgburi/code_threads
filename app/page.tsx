b5833d457f4015b46822e8b084d2a989f8ba8467
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-14 10:21:13 -0400

    Refactor page component to include session check and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import AsideBar from "@/components/SideBar/AsideBar";
 import Feed from "@/components/Feed";
 import Header from "@/components/Header";
 import React from "react";
-import { AnimatedModal } from "@/components/Modal";
+import { useSession } from "next-auth/react";
 
 const Page = () => {
+  const session = useSession();
+  console.log(session);
+
   return (
     <main className="flex h-screen">
       {/* Sidebar for larger screens */}
@@ -14,16 +17,35 @@ const Page = () => {
 
       {/* Main container centered with max-width */}
       <div className="container">
-        <div className="flex-column">
-          <AnimatedModal />
+        {/* For You Section */}
+        <section className="flex-column">
           <Header title="For You" />
           <Feed>awad</Feed>
-        </div>
+        </section>
 
-        <div className="flex-column">
-          <Header title="Thread" />
-          <Feed>hdadwa</Feed>
-        </div>
+        {/* User Profile Section */}
+
+        {session?.data?.user ? (
+          <section className="flex-column">
+            <Header title="Thread" />
+            <Feed>hdadwa</Feed>
+          </section>
+        ) : (
+          /**
+           * todo: find a skeleton loader for this section but still
+           * todo: display the message "Please log in to view your profile."
+           */
+          <section className="flex-column">
+            <Header title="Thread" />
+            <div className="bg-[#181818] border p-8 rounded-3xl h-full overflow-auto">
+              <div className="flex items-center justify-center h-full">
+                <p className="text-lg font-bold text-gray-500">
+                  Please log in to view your profile.
+                </p>
+              </div>
+            </div>
+          </section>
+        )}
       </div>
     </main>
   );
