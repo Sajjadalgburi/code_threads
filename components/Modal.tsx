53ca25f5cb3486b99314fbe57ea5a63af12e6c16
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-16 08:52:11 -0400

    Refactor Modal component to include user profile picture and name in the reply section

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
 import { Modal, ModalBody, ModalTrigger } from "./ui/animated-modal";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export function AnimatedModal() {
+  const { data: session } = useSession();
+
   return (
     <div className="py-40  flex items-center justify-center">
       <Modal>
@@ -11,16 +15,28 @@ export function AnimatedModal() {
         </ModalTrigger>
         <ModalBody>
           {/* Title */}
-          <h3 className="text-sm md:text-xl capitalize text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
+          <h3 className="text-sm md:text-xl capitalize text-neutral-600 dark:text-neutral-100 font-bold text-center mb-3">
             New Thread
           </h3>
 
           {/* Body */}
           <div className="flex justify-start items-center gap-4">
-            <div className="bg-slate-500 w-[100px] h-[100px]"></div>
-            <div className="bg-slate-500 w-[100px] h-[100px]"></div>
+            <Image
+              src={session?.user?.image as string}
+              alt="profile picture"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <div>
+              <p className="text-sm dark:text-white font-semibold">
+                {session?.user?.name}
+              </p>
+              <input type="text" />
+            </div>
           </div>
 
+          {/* Modal Footer */}
           <div className="flex justify-between items-end mt-14">
             <div className="flex justify-start">
               <p className="text-gray-600 text-sm">Anyone can reply...</p>
