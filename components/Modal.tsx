3eb58fbc73007a2e12591ab9e194ee024e0f8d8d
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-16 18:02:07 -0400

    Refactor Modal component to use DefaultModal instead of AnimatedModal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import React from "react";
 import { Modal, ModalBody, ModalTrigger } from "./ui/animated-modal";
@@ -5,52 +6,40 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import ThreadForm from "./ThreadForm";
 
-export function AnimatedModal() {
+export function DefaultModal({ action }: { action: string }) {
   const { data: session } = useSession();
 
   return (
-    <div className="py-40  flex items-center justify-center">
+    <div className="py-40 flex items-center justify-center">
       <Modal>
         <ModalTrigger className="bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn">
-          <span className="text-center">Create</span>
+          <span className="text-center">
+            {action === "create" ? "Create" : "Edit"}
+          </span>
         </ModalTrigger>
         <ModalBody>
           {/* Title */}
           <h3 className="text-sm md:text-xl capitalize text-neutral-600 dark:text-neutral-100 font-bold text-center mb-3">
-            New Thread
+            {action === "create" ? "New Thread" : "Edit Thread"}
           </h3>
 
           {/* Body */}
-          <div className="flex justify-start items-center gap-4">
-            <Image
-              src={session?.user?.image as string}
-              alt="profile picture"
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            <div className="flex flex-col w-full flex-wrap">
+          <div className="flex flex-col justify-center gap-4">
+            <div className="flex justify-right items-center gap-3">
+              <Image
+                src={session?.user?.image as string}
+                alt="profile picture"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
               <p className="text-md dark:text-white font-semibold">
                 {session?.user?.name}
               </p>
-              <ThreadForm />{" "}
             </div>
+            <ThreadForm action={action} />{" "}
+            {/* Pass data to ThreadForm if needed */}
           </div>
-
-          {/* Modal Footer */}
-          {/* <div className="flex justify-between items-end mt-14">
-            <div className="flex justify-start">
-              <p className="text-gray-600 text-sm">Anyone can reply...</p>
-            </div>{" "}
-            <div className="flex gap-2 ">
-              <button className="px-2 py-1 bg-red-500 text-white dark:bg-red-500 dark:border-red-500 border border-red-500 rounded-md text-sm w-28">
-                Cancel
-              </button>
-              <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
-                Post
-              </button>
-            </div>
-          </div> */}
         </ModalBody>
       </Modal>
     </div>
