47a1481ad38e10da22dac865bb07f4853970dbb6
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-17 10:00:38 -0400

    Refactor route to remove unused code and pass req and res to getServerSession

--- app/api/thread/new/route.ts
+++ app/api/thread/new/route.ts
@@ -1,9 +1,6 @@
 import { connectToDatabase } from "@/lib/database/mongoose";
-import User from "@/lib/models/user.model";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Thread from "@/lib/models/thread.model";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
 
 /**
  * Route to create a new thread.
@@ -12,17 +9,7 @@ import { authOptions } from "@/lib/authOptions";
 export const POST = async () => {
   try {
     await connectToDatabase();
-
-    const data = await getServerSession(authOptions);
-
-    // finding a user by their email and retrieving their id
-    const user = await User.findOne({ email: data?.user.email });
-    if (!user) {
-      new Response("User not found...", { status: 404 });
-      return;
-    }
-
-    console.log("post success: ", user);
+    // Pass req and res to getServerSession
 
     return new Response("New thread created", { status: 200 });
   } catch (error) {
