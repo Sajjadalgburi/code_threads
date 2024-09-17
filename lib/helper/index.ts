69468a7527d140a4bef1456c02cef661ab92cd66
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-17 11:29:21 -0400

    Refactor helper/index.ts to add code snippet language detection

--- lib/helper/index.ts
+++ lib/helper/index.ts
@@ -1,5 +1,11 @@
 import User from "../models/user.model";
+import hljs from "highlight.js";
 
 export const getUserByEmail = async (email: string) => {
   return await User.findOne({ email });
+};
+
+export const detectLanguage = (codeSnippet: string): string => {
+  const detectedLanguage = hljs.highlightAuto(codeSnippet).language;
+  return detectedLanguage || "plaintext"; // Fallback to plaintext if no language detected
 };
