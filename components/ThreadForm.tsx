818c3a2dc8a92b73ed8656a25a72c25977235799
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-17 11:34:00 -0400

    Refactor ThreadForm component to include code field, handle form submission, and improve code snippet language detection

--- components/ThreadForm.tsx
+++ components/ThreadForm.tsx
@@ -8,61 +8,25 @@ import { Button } from "./ui/button";
 import { IThread } from "@/interfaces";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { detectLanguage } from "@/lib/helper";
 
 const ThreadForm = ({ action }: { action: string }) => {
-  // Use State's
   const [thread, setThread] = useState<string>("");
   const [code, setCode] = useState<string | null>("");
   const [language, setLanguage] = useState<string>("");
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const router = useRouter();
+  const session = useSession();
+  const userId = session?.data?.user.id;
 
   // Detect the language of the code
-  const detectLanguage = (codeSnippet: string) => {
-    const detectedLanguage = hljs.highlightAuto(codeSnippet).language;
-    setLanguage(detectedLanguage || "plaintext"); // Fallback to plaintext if no language detected
-  };
-
-  const router = useRouter();
 
   // Update the code and detect language on change
   const handleCodeChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newCode = e.target.value;
     setCode(newCode);
-    detectLanguage(newCode);
+    setLanguage(detectLanguage(newCode));
   };
-
-  const session = useSession();
-  const userId = session?.data?.user.id;
-
-  // use Effect to make a post to api backend /api/thread/new
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       if (!userId) return console.error("User not found...");
-
-  //       const response = await fetch("/api/thread/new", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ${userId}`,
-  //         },
-
-  //         body: JSON.stringify({ text: code }),
-  //       });
-
-  //       if (response.ok) {
-  //         const data: IThread = await response.json();
-  //         console.log(data);
-  //       } else {
-  //         console.error("Failed to fetch data");
-  //       }
-  //     } catch (error) {
-  //       console.error(`${action} failed: `, error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [action, code, userId]);
 
   const handleSubmit = async () => {
     try {
