af2427337887c3306d446c76112e939b784ec9c5
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-17 10:49:53 -0400

    Refactor route to include user ID in JWT token and MongoDB _id in session

--- app/api/thread/new/route.ts
+++ app/api/thread/new/route.ts
@@ -1,16 +1,39 @@
 import { connectToDatabase } from "@/lib/database/mongoose";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Thread from "@/lib/models/thread.model";
+import { NextRequest } from "next/server";
+import { redirect } from "next/navigation";
 
 /**
  * Route to create a new thread.
  * Need to pass user data such as first finding their id and using it to create a new thread.
  */
-export const POST = async () => {
+
+export const POST = async (req: NextRequest) => {
   try {
     await connectToDatabase();
-    // Pass req and res to getServerSession
 
+    const { text, code } = await req.json();
+
+    // grab the user id from the Authorization header
+    const userId = req.headers.get("Authorization")?.slice(7) as string;
+    if (!userId) {
+      return new Response("User not found...", { status: 404 });
+    }
+
+    const newThread = await Thread.create({
+      text: text,
+      code: code,
+      user: userId,
+    });
+
+    if (!newThread) {
+      return new Response("Failed to create a new thread", { status: 500 });
+    }
+    await newThread.save();
+    setTimeout(() => {
+      redirect("/");
+    }, 1000);
     return new Response("New thread created", { status: 200 });
   } catch (error) {
     console.error(error);
