6c1edf655f50d4bfdaf0606cf9f1371b2fefc5c1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-18 09:18:06 -0400

    Refactor animated-modal.tsx to remove unnecessary code

--- components/ui/animated-modal.tsx
+++ components/ui/animated-modal.tsx
@@ -54,7 +54,7 @@ export const ModalTrigger = ({
   return (
     <button
       className={cn(
-        "px-4 py-2 rounded-md text-black dark:text-white text-center relative overflow-hidden",
+        "px-4 py-4 rounded-md text-black dark:text-white text-center",
         className
       )}
       onClick={() => setOpen(true)}
