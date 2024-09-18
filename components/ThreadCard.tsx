ea54c6d7fddaecaef0bffbf38df5676f719d1796
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-18 10:59:13 -0400

    Refactor ThreadCard component to add like and comment buttons

--- components/ThreadCard.tsx
+++ components/ThreadCard.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Image from "next/image";
-import { MessageCircle } from "lucide-react";
-import { Heart } from "lucide-react";
+import { MessageCircle, Heart } from "lucide-react";
+import { useState } from "react";
 
 const ThreadCard = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLiked = async () => {
+    setLiked(!liked);
+  };
+
   return (
-    <div className="bg-slate-500 flex justify-start p-2">
-      <div className="mr-2">
-        {" "}
+    <div className="bg-gray-200 dark:bg-transparent rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transition-shadow duration-300">
+      <div className="flex items-start space-x-4">
         <Image
           src="/app-logo.png"
           alt="user profile"
@@ -15,40 +20,43 @@ const ThreadCard = () => {
           height={50}
           className="rounded-full"
         />
-      </div>
 
-      {/* rest of the content */}
-      <div>
-        {/* card header */}
-        <div className="flex gap-4 items-center">
-          {/* users name will be a link */}
-          <h4 className="text-md md:text-lg font-bold">username</h4>
-          <p className=" text-gray-400 text-sm font-extralight">20h</p>
-        </div>
+        <div className="flex-1">
+          {/* Card Header */}
+          <div className="flex items-center justify-between mb-2">
+            <h4 className="text-md md:text-lg font-bold text-black dark:text-white">
+              username
+            </h4>
+            <p className="text-gray-500 dark:text-gray-400 text-xs md:text-sm">
+              20h
+            </p>
+          </div>
 
-        {/* card body */}
-        <div className="my-2">
-          <p className="font-extralight text-xs md:text-sm tracking-wide">
+          {/* Card Body */}
+          <p className="text-sm md:text-base text-gray-700 dark:text-gray-300 mb-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
           </p>
-        </div>
 
-        {/* card stats */}
-        <div className="flex">
-          <button className="hover:bg-slate-300/10 transition duration-300 px-3 py-2 rounded-3xl">
-            {" "}
-            <div className="flex justify-center items-center gap-1">
-              <Heart size={20} />
-              <p>22</p>
-            </div>
-          </button>
-          <button className="hover:bg-slate-300/10 transition duration-300 px-3 py-2 rounded-3xl">
-            {" "}
-            <div className="flex justify-center items-center gap-1">
+          {/* Card Stats */}
+          <div className="flex space-x-6">
+            <button
+              onClick={handleLiked}
+              className={`flex items-center space-x-2 ${
+                liked === false
+                  ? "text-gray-600 dark:text-gray-400"
+                  : "text-transparent"
+              } text-transparent transition-colors duration-300`}
+            >
+              <Heart fill={`${liked && "red"}`} size={20} />
+              <p className="text-gray-600 dark:text-gray-400 hover:text-black">
+                22
+              </p>
+            </button>
+            <button className="flex items-center space-x-2 text-gray-600 dark:text-gray-400 hover:text-black transition-colors duration-300">
               <MessageCircle size={20} />
               <p>22</p>
-            </div>
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     </div>
