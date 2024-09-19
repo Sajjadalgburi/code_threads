dce52731409706ec43536ce8b6f5387c44f1c31a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-19 13:08:57 -0400

    Refactor ThreadCard component to include user image based on user data and action

--- components/ThreadCard_Things/ThreadCard.tsx
+++ components/ThreadCard_Things/ThreadCard.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import Image from "next/image";
 import { MessageCircle, Heart } from "lucide-react";
 import { useState } from "react";
-import { IThread } from "@/interfaces";
+import { IThread, UserInterface } from "@/interfaces";
 import moment from "moment"; // for formatting the date
 import StatButtons from "./StatButtons";
 
 interface ThreadCardProps {
   threadData: IThread;
   action: "main_feed" | "profile_feed";
+  users: UserInterface[];
 }
 
 const ThreadCard: React.FC<ThreadCardProps> = ({
   threadData,
+  users,
   action = "main_feed",
 }) => {
   const [liked, setLiked] = useState<boolean>(false);
@@ -29,11 +31,18 @@ const ThreadCard: React.FC<ThreadCardProps> = ({
   // Format createdAt to relative time (e.g., '20h ago')
   const formattedTime = moment(threadData.createdAt).fromNow();
 
+  console.log(users);
+  console.log(threadData);
+
   return (
     <div className="bg-gray-200 dark:bg-transparent rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transition-shadow duration-300">
       <div className="flex items-start space-x-4">
         <Image
-          src="/app-logo.png"
+          src={
+            users.find(
+              (u) => (u._id as string) === (threadData.user as unknown)
+            )?.image || "/default-avatar.jpg"
+          }
           alt="user profile"
           width={50}
           height={50}
@@ -44,7 +53,11 @@ const ThreadCard: React.FC<ThreadCardProps> = ({
           {/* Card Header */}
           <div className="flex items-center justify-between mb-2">
             <h4 className="text-md md:text-lg font-bold text-black dark:text-white">
-              {action === "profile_feed" ? "You" : threadData.user}
+              {action === "profile_feed"
+                ? "You"
+                : users.find(
+                    (u) => (u._id as string) === (threadData.user as unknown)
+                  )?.username || "Unknown"}
             </h4>
             <p className="text-gray-500 dark:text-gray-400 text-xs md:text-sm">
               {formattedTime}
